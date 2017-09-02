a28180970bc25ea2fb2d05d7174beb965432e76c
xywang <125479558@qq.com>
2017-09-03 00:45:39 +0800

    增加admin監控

---
+++ spring-admin-web/src/main/java/com/iflytek/springadminweb/SpringAdminWebApplication.java
@@ -0,0 +1,16 @@
+package com.iflytek.springadminweb;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
+public class SpringAdminWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAdminWebApplication.class, args);
+	}
+}
---
+++ spring-admin-web/src/test/java/com/iflytek/springadminweb/SpringAdminWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iflytek.springadminweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAdminWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
